e540bde48e9481814ff921c5c71148517ab91b29
Mykhaylo Kuzmin <xxxxxxxxx@gmail.com>
2023-04-22 22:33:57 -0400

    FIX: Update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::include('partials.svgs');
+        \Illuminate\Support\Facades\Response::macro('svg', function ($path, $status = 200, array $headers = []) {
+            $contents = file_get_contents($path);
+            $response = new \Illuminate\Http\Response($contents, $status, $headers);
+            $response->header('Content-Type', 'image/svg+xml');
+            return $response;
+        });
+        // add the following line
+        \Illuminate\Http\Response::macro('allowedSvg', function ($content) {
+            $content->header('Content-Type', 'image/svg+xml');
+            return $content;
+        });
     }
 }
--- start.php
+++ start.php
@@ -1,6 +1,3 @@
 <?php
-// Start the Laravel development server
-exec('php artisan serve --host=0.0.0.0 --port=8080 > /dev/null 2>&1 &');
-
 // Start the Laravel queue worker
 exec('php artisan queue:work > /dev/null 2>&1 &');
