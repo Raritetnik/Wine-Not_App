acd1dceb0098f4a0c4e729a157392384f659acf0
Lbarreiros1980 <88776130+Lbarreiros1980@users.noreply.github.com>
2023-04-26 14:30:48 -0400

    home blade revenu à son original

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,65 +1,39 @@
 @extends('layouts.app')
 @section('content')
-<div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-<div class="container mx-auto">
+<div class="container">
   <header class="mb-8">
     <a href="/" class="text-accent_wine uppercase tracking-wide font-bold">
       <img src="{{ asset('img/svg/logoWn.svg') }}" alt="logo-wineNot" class="mx-auto" width="120">
     </a>
   </header>
-  <section class="mx-4 px-6 flex flex-col mb-4">
-    <div class="flex flex-col space-y-4 md:flex-row md:space-x-6 md:space-y-0">
-      <div class="formbold-main-wrapper">
-          <div class="formbold-form-wrapper">
-          <form action="{{route('bouteilles.insererBouteille')}}" method="post" enctype="multipart/form-data" class="w-full">
-               <!-- ajouter un token pour autoriser la route une seconde fois -->
-               @csrf
-      <div class="w-full md:w-1/2 mb-3">
-          <label for="nom" class="block text-gray-700 font-bold mb-2">Ajoutez une bouteille</label>
-          <input
-          type="nom"
-          name="nom"
-          id="nom"
-          placeholder="nom de la bouteille"
-          class="block w-full py-2 px-3 rounded-md border border-gray-300 focus:border-purple-500 focus:outline-none"
-          />
-      </div>
-      <div class="w-full md:w-1/2 mb-3">
-          <label for="quantite" class="block text-gray-700 font-bold mb-2">Quantité</label>
-          <input class="block w-full py-2 px-3 rounded-md border border-gray-300 focus:border-purple-500 focus:outline-none" name="quantité" id="quantité" placeholder="Quantité">
-          </input>
+  <section class="mx-4 px-6 flex flex-col gap-6 mb-8">
+    <div class="flex items-center justify-around relative">
+      <h2 class="font-black text-gray-600 text-xl" style="max-width: 15ch">Choisissez votre vin à la SAQ!</h2>
+      <div class="acc_icon p-5 border rounded-lg flex justify-center" style="width: 100px; height: 100px; border-color: #ABA08D">
+        <img src="{{ asset('img/svg/full_bottle.svg') }}" alt="">
+        <div class="absolute bottom-0 text-4xl" style="color: #ABA08D; transform: translateY(80%)">&#8595;</div>
       </div>
     </div>
-    <div class="mb-6">
-      <label for="date_achat" class="block text-gray-700 font-bold mb-2">Date d'achat</label>
-      <div class="flex flex-col md:flex-row md:space-x-4">
-        <input
-          type="date_achat"
-          name="date_achat"
-          id="date_achat"
-          placeholder="Date d'achat"
-          class="block w-full py-2 px-3 rounded-md border border-gray-300 focus:border-purple-500 focus:outline-none mb-2 md:mb-0 md:w-2/5 mb-3"
-        />
+    <div class="flex items-center justify-around relative">
+      <h2 class="font-black text-gray-600 text-xl" style="max-width: 15ch">Localisez la bouteille dans notre application</h2>
+      <div class="acc_icon p-5 border rounded-lg flex justify-center" style="width: 100px; height: 100px; border-color: #ABA08D">
+        <img src="{{ asset('img/svg/form-icon.svg') }}" alt="">
+        <div class="absolute bottom-0 text-4xl" style="color: #ABA08D; transform: translateY(80%)">&#8595;</div>
       </div>
-        <label for="phone" class="block text-gray-700 font-bold mb-2">Valide jusqu'à</label>
-        <div class="flex flex-col md:flex-row md:space-x-4">
-          <input
-          type="date"
-          name="date_achat"
-          id="date_achat"
-          placeholder="Valide jusqu'à"
-          class="block w-full py-2 px-3 rounded-md border border-gray-300 focus:border-purple-500 focus:outline-none mb-2 md:mb-0 md:w-3/5"
-        />
-        </div>
     </div>
-    <div class="w-full md:w-1/2 mb-3">
-      <label class="block text-gray-700 font-bold mb-2">Cellier</label>
-      <select class="block w-full py-2 px-3 rounded-md border border-gray-300 focus:border-purple-500 focus:outline-none" name="cellier" id="cellier">
-        <option value="male">cellier 1</option>
-        <option value="female">cellier 2</option>
-        <option value="others">cellier 3</option>
-      </select>
-  </div>
+    <div class="flex items-center justify-around relative">
+      <h2 class="font-black text-gray-600 text-xl" style="max-width: 15ch">Attribuez-lui un cellier !</h2>
+      <div class="acc_icon p-5 border rounded-lg flex justify-center" style="width: 100px; height: 100px; border-color: #ABA08D">
+        <img src="{{ asset('img/svg/barrols.svg') }}" alt="">
+        <div class="absolute bottom-0 text-4xl" style="color: #ABA08D; transform: translateY(80%)">&#8595;</div>
+      </div>
+    </div>
+    <div class="flex items-center justify-around relative">
+      <h2 class="font-black text-gray-600 text-xl" style="max-width: 15ch">Gérez votre cave à vin comme un pro !</h2>
+      <div class="acc_icon p-5 border rounded-lg flex justify-center" style="width: 100px; height: 100px; border-color: #ABA08D">
+        <img src="{{ asset('img/svg/empty_bottle.svg') }}" alt="">
+      </div>
+    </div>
   </section>
   <footer class="flex flex-col items-center mb-8 mx-10">
     <a href="/register" class="text-white py-2 w-full rounded-md mb-2 flex justify-center" style="background-color: #67375C">Commencer</a>
@@ -68,7 +42,5 @@
       <a href="/login" style="color: #67375C">Connecter</a>
     </small>
   </footer>
-</div>
-</div>
 </div>
 @endsection
