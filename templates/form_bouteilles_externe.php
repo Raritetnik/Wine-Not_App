d51e6c49be7534e985151c323421c38893294802
Lbarreiros1980 <88776130+Lbarreiros1980@users.noreply.github.com>
2023-04-26 16:23:50 -0400

    derniers changements du form bouteilles externe

--- templates/form_bouteilles_externe.php
+++ templates/form_bouteilles_externe.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 @section('content')
 
+<main class="py-4">
 <div class="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-<div class="container mx-auto">
-  <header class="mb-8">
-    <a href="/" class="text-accent_wine uppercase tracking-wide font-bold">
-      <img src="{{ asset('img/svg/logoWn.svg') }}" alt="logo-wineNot" class="mx-auto" width="120">
-    </a>
-  </header>
-  <section class="mx-4 px-6 flex flex-col mb-4">
-    <div class="flex flex-col space-y-4 md:flex-row md:space-x-6 md:space-y-0">
-      <div class="formbold-main-wrapper">
-          <div class="formbold-form-wrapper">
-          <form action="{{route('bouteilles.insererBouteille')}}" method="post" enctype="multipart/form-data" class="w-full">
-               <!-- ajouter un token pour autoriser la route une seconde fois -->
+  <div class="container mx-auto">
+    <header class="mb-8">
+      <a href="/" class="text-accent_wine uppercase tracking-wide font-bold">
+        <img src="{{ asset('img/svg/logoWn.svg') }}" alt="logo-wineNot" class="mx-auto" width="120">
+      </a>
+    </header>
+    <section class="mx-4 px-6 flex flex-col mb-4">
+      <div class="flex flex-col space-y-4 md:flex-row md:space-x-6 md:space-y-0">
+        <div class="">
+            <div class="">
+            <form action="{{route('bouteilles.insererBouteille')}}" method="post" enctype="multipart/form-data" class="w-full"> <!-- ajouter un token pour autoriser la route une seconde fois -->
                @csrf
       <div class="w-full md:w-1/2 mb-3">
           <label for="nom" class="block text-gray-700 font-bold mb-2">Ajoutez une bouteille</label>
@@ -31,7 +31,7 @@
           </input>
       </div>
     </div>
-    <div class="mb-6">
+    <div class="mb-2">
       <label for="date_achat" class="block text-gray-700 font-bold mb-2">Date d'achat</label>
       <div class="flex flex-col md:flex-row md:space-x-4">
         <input
@@ -42,13 +42,13 @@
           class="block w-full py-2 px-3 rounded-md border border-gray-300 focus:border-purple-500 focus:outline-none mb-2 md:mb-0 md:w-2/5 mb-3"
         />
       </div>
-        <label for="phone" class="block text-gray-700 font-bold mb-2">Valide jusqu'à</label>
+        <label for="garde_jusqua" class="block text-gray-700 font-bold mb-2">Gardez jusqu'à</label>
         <div class="flex flex-col md:flex-row md:space-x-4">
           <input
           type="date"
-          name="date_achat"
-          id="date_achat"
-          placeholder="Valide jusqu'à"
+          name="garde_jusqua"
+          id="garde_jusqua"
+          placeholder="Gardez jusqu'à"
           class="block w-full py-2 px-3 rounded-md border border-gray-300 focus:border-purple-500 focus:outline-none mb-2 md:mb-0 md:w-3/5"
         />
         </div>
@@ -56,20 +56,20 @@
     <div class="w-full md:w-1/2 mb-3">
       <label class="block text-gray-700 font-bold mb-2">Cellier</label>
       <select class="block w-full py-2 px-3 rounded-md border border-gray-300 focus:border-purple-500 focus:outline-none" name="cellier" id="cellier">
-        <option value="male">cellier 1</option>
-        <option value="female">cellier 2</option>
-        <option value="others">cellier 3</option>
+        <option value="celliers.id.nom">cellier 1</option>
+        <option value="">cellier 2</option>
+        <option value="">cellier 3</option>
       </select>
   </div>
   </section>
   <footer class="flex flex-col items-center mb-8 mx-10">
-    <a href="/register" class="text-white py-2 w-full rounded-md mb-2 flex justify-center" style="background-color: #67375C">Commencer</a>
+    <a href="/register" class="text-white py-2 w-full rounded-md mb-2 flex justify-center" style="background-color: #67375C">Ajouter</a>
     <small style="color: #909090">
-      Avez-vous déjà un compte?
-      <a href="/login" style="color: #67375C">Connecter</a>
+      <a href="/login" style="color: #67375C">Previous</a>
     </small>
   </footer>
+  </div>
+  </div>
+</main>
 </div>
-</div>
-</div>
-@endsection
+  @endsection
