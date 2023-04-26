d20013aeafbdcea1a3b8c172c0822f79bdcbb155
Lbarreiros1980 <88776130+Lbarreiros1980@users.noreply.github.com>
2023-04-26 14:46:14 -0400

    changement du form

--- templates/form_bouteilles_externe.php
+++ templates/form_bouteilles_externe.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 @section('content')
 
-
 <div class="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
 <div class="container mx-auto">
   <header class="mb-8">
