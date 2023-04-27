46c02698ca8df0c5d43cd2cebc09fb6907bcaf06
JulieDeRen <julie.deschenes-renaud@hec.ca>
2023-04-26 21:21:25 -0400

    Modification de la ligne 62 pour la personne qui fait créer-bouteille et qui avait repris la route creer-cellier

--- routes/web.php
+++ routes/web.php
@@ -59,7 +59,7 @@ Route::put('/celliers/{cellier}/{idbouteille}', [App\Http\Controllers\CellierCon
 Route::get('/bouteille/{idBouteille}', [BouteilleController::class, 'show'])->name('bouteille.show')->middleware('auth');
 Route::get('/bouteilles', [BouteilleController::class, 'listeBouteilles'])->name('bouteilles');
 Route::get('/creer-bouteille', [App\Http\Controllers\BouteilleController::class, 'creer'])->name('bouteilles.creer')->middleware('auth');
-Route::post('/creer-cellier', [App\Http\Controllers\BouteilleController::class, 'insererBouteille'])->name('bouteilles.insererBouteille')->middleware('auth');
+Route::post('/creer-bouteille', [App\Http\Controllers\BouteilleController::class, 'insererBouteille'])->name('bouteilles.insererBouteille')->middleware('auth');
 /**
  * Les liens de Script Loader SAQ
  */
