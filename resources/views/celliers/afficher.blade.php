7776a4539a4b188d1c30bd255f038a32876cb100
JulieDeRen <julie.deschenes-renaud@hec.ca>
2023-04-27 17:52:31 -0400

    Remettre le script sql au controlleur pour afficher correctement

--- resources/views/celliers/afficher.blade.php
+++ resources/views/celliers/afficher.blade.php
@@ -68,7 +68,7 @@
     @endforeach
 </section>
 </div>
-<v-filtre :type="{{$type}}" :pays="{{$pays}}" :cellier="{{$cellier}}" :bouteilles="{{$bouteillesJulie}}"/>
+<v-filtre :type="{{$type}}" :pays="{{$pays}}" :cellier="{{$cellier}}" />
 </div>
 
 @endsection
