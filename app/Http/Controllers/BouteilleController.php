b8c8399cd80399a42331bdfa589bbd93e68e96d1
Lbarreiros1980 <88776130+Lbarreiros1980@users.noreply.github.com>
2023-05-01 14:46:01 -0400

    func modifierBouteille

--- app/Http/Controllers/BouteilleController.php
+++ app/Http/Controllers/BouteilleController.php
@@ -157,11 +157,8 @@ class BouteilleController extends Controller
     {
         //
     }
-
-<<<<<<< HEAD
-
-
-public function modifierBouteille(Bouteille_Par_Cellier $idBouteille)
+    
+    public function modifierBouteille(Bouteille_Par_Cellier $idBouteille)
     {
         //return $idBouteille;
 
@@ -172,48 +169,8 @@ public function modifierBouteille(Bouteille_Par_Cellier $idBouteille)
         ->get();
 
         return $bouteilleModifie;
-
-
-
-        // Vérifier que la bouteille appartient bien à l'utilisateur connecté
-        // $utilisateur_id = auth()->user()->id;
-        // $cellier_id = $bouteille->vino_cellier_id;
-        // $bouteille = $vino_bouteille_id;
-        // if (!$bouteille->cellier->estProprietaire($utilisateur_id)) {
-        //     abort(403, 'Unauthorized action.');
-        // }
->>>>>>> master
-        
-        // // Valider les données du formulaire
-        // $request->validate([
-        //     'nom' => 'required|min:5|max:100',
-        //     'date_achat' => 'required|date',
-        //     'quantite' => 'required|integer|min:1',
-        //     'prix' => 'required|numeric|min:0',
-        //     'millesime' => 'nullable|integer|min:1900|max:' . date('Y'),
-        //     'garde_jusqua' => 'nullable|date',
-        // ]);
-    
-        // // Mettre à jour la bouteille et la sauvegarder
-        // $bouteille->vino_bouteille->update([
-        //     'nom' => $request->nom,
-        //     'prix_saq' => $request->prix,
-        //     'vino_format_id' => $request->vino_format_id,
-        //     'vino_type_id' => $request->vino_type_id,
-        //     'pays_id' => $request->pays_id,
-        // ]);
-        // $bouteille->update([
-        //     'date_achat' => $request->date_achat,
-        //     'quantite' => $request->quantite,
-        //     'prix' => $request->prix * $request->quantite,
-        //     'millesime' => $request->millesime,
-        //     'garde_jusqua' => $request->garde_jusqua,
-        // ]);
-    
-        // Rediriger vers la page du cellier contenant la bouteille modifiée
-        return redirect(route('celliers.afficher', $cellier_id));
     }
-    
+
     /**
      * Remove the specified resource from storage.
      *
