31dca760d128ed6978c6a0181acc05dee94fdd15
Lbarreiros1980 <88776130+Lbarreiros1980@users.noreply.github.com>
2023-05-11 15:20:20 -0400

    substituition des icones et fix tab bar

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,25 +1,25 @@
 <footer class="bg-accent_wine px-4 relative">
   <div class="bg-accent_wine px-4 relative">
     <div class="flex justify-around py-4">
-      <div class="max-w-45ch flex justify-center flex-wrap gap-4 py-5 px-2 mt-2">
-        <a href="/favoris" class="text-white pt-6 text-lg flex flex-col items-center">
-          <img src="{{ asset('img/svg/logout2.svg') }}" alt="menu" class="inline-block align-middle">
+      <div class="max-w-39ch flex justify-center flex-wrap gap-7 py-5 px-2 mt-2">
+        <a href="/favoris" class="text-white pt-3 text-lg flex flex-col items-center">
+          <img src="{{ asset('img/svg/log-out.svg') }}" alt="menu" class="inline-block align-middle pb-2">
           <span class="inline-block align-middle">Quitter</span>
         </a>
-        <a href="/favoris" class="text-white pt-6 text-lg flex flex-col items-center">
-          <img src="{{ asset('img/svg/coeur2.svg') }}" alt="menu" class="inline-block align-middle">
+        <a href="/favoris" class="text-white pt-3 text-lg flex flex-col items-center">
+          <img src="{{ asset('img/svg/heart.svg') }}" alt="menu" class="inline-block align-middle pb-2">
           <span class="inline-block align-middle">Favoris</span>
         </a>
-        <a href="/favoris" class="text-white pt-6 text-lg flex flex-col items-center">
-          <img src="{{ asset('img/svg/clock.svg') }}" alt="menu" class="inline-block align-middle">
+        <a href="/favoris" class="text-white pt-3 text-lg flex flex-col items-center">
+          <img src="{{ asset('img/svg/clock2.svg') }}" alt="menu" class="inline-block align-middle pb-3">
           <span class="inline-block align-middle">Historique</span>
         </a>
-        <a href="/favoris" class="text-white pt-6 text-lg flex flex-col items-center">
-          <img src="{{ asset('img/svg/userNew.svg') }}" alt="menu" class="inline-block align-middle">
+        <a href="/favoris" class="text-white pt-3 text-lg flex flex-col items-center">
+          <img src="{{ asset('img/svg/user2.svg') }}" alt="menu" class="inline-block align-middle pb-2">
           <span class="inline-block align-middle">Profil</span>
         </a>
-        <a href="/favoris" class="text-white pt-6 text-lg flex flex-col items-center">
-          <img src="{{ asset('img/svg/more.svg') }}" alt="menu" class="inline-block align-middle">
+        <a href="/favoris" class="text-white pt-3 text-lg flex flex-col items-center">
+          <img src="{{ asset('img/svg/more2.svg') }}" alt="menu" class="inline-block align-middle pt-0.5 pb-2">
           <span class="inline-block align-middle">More</span>
         </a>
       </div>
