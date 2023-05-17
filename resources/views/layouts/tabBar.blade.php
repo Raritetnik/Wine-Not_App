9c6568f7cf5218d61639f27d826ccc82759ee42f
Lbarreiros1980 <88776130+Lbarreiros1980@users.noreply.github.com>
2023-05-17 15:32:17 -0400

    tabBar avec icones remis en place

--- resources/views/layouts/tabBar.blade.php
+++ resources/views/layouts/tabBar.blade.php
@@ -16,9 +16,9 @@
       <img src="{{ asset('img/svg/heart.svg') }}" alt="menu" class="inline-block align-middle pb-2">
       <span class="inline-block align-middle">Favoris</span>
     </a>
-    <a href="" class="tab-icon">
-      <img src="{{ asset('img/svg/clock2.svg') }}" alt="menu" class="inline-block align-middle pb-2">
-      <span class="inline-block align-middle">Historique</span>
+    <a href="{{route('celliers.index') }}" class="tab-icon">
+      <img src="{{ asset('img/svg/celliers3.svg') }}" alt="menu" class="inline-block align-middle pb-2" aria-label="Celliers" title="Celliers">
+      <span class="inline-block align-middle">Mes Celliers</span>
     </a>
     <a href="/compte" class="tab-icon transition-colors duration-200 hover:text-secondary" aria-label="Compte" title="Compte">
       <img src="{{ asset('img/svg/user2.svg') }}" class="inline-block align-middle pb-2">
